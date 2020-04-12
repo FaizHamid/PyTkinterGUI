67aba852d3592d4b1270b67da9753522b19a4d1f
FaizHamid <hamidmfaiz@gmail.com>
2020-04-11 20:35:16 -0400

    Open File Dialog Box

---
+++ OpenFileDialogBox.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+
+root = Tk()
+root.title('Open File Dialog Box in Netflix')
+root.iconbitmap('/Users/faizhamid/PycharmProjects/PyTkinterGUI/images/Netflix.ico')
+
+
+# root.filename = filedialog.askopenfilename(initialdir="/Users/faizhamid/PycharmProjects/PyTkinterGUI/images", title="Select a File", filetypes=(("png files", "*.png"), ("all files", "*.*")))
+# my_label = Label(root, text=root.filename).pack()
+# my_image = ImageTk.PhotoImage(Image.open(root.filename))
+# my_image_label = Label(image=my_image).pack()
+
+def open():
+    global my_image
+    root.filename = filedialog.askopenfilename(initialdir="/Users/faizhamid/PycharmProjects/PyTkinterGUI/images",
+                                               title="Select a File",
+                                               filetypes=(("png files", "*.png"), ("all files", "*.*")))
+    my_label = Label(root, text=root.filename).pack()
+    my_image = ImageTk.PhotoImage(Image.open(root.filename))
+    my_image_label = Label(image=my_image).pack()
+
+
+my_btn = Button(root, text="Open File", command=open).pack()
+
+root.mainloop()
